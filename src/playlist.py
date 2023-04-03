24e197c2aa580925f2d5cbb7c1df3a0564f02bd9
sergey <szaprudnov@hotmail.com>
2023-04-03 21:36:03 +0300

    add class PlayList

--- src/playlist.py
+++ src/playlist.py
@@ -4,3 +4,14 @@ import isodate
 from src.video import Channel
 from googleapiclient.discovery import build
 
+
+class PlayList:
+    def __init__(self, id_playlist):
+        self.id_playlist = id_playlist
+        self.url = 'https://www.youtube.com/playlist?list=' + self.id_playlist
+        self.youtube = Channel.get_service().playlistItems().list(
+            playlistId=self.id_playlist,
+            part='snippet,contentDetails,id,status',
+            maxResults=10,
+        ).execute()
+
