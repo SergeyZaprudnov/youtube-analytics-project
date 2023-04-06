05a69a89d06a736fd60a84f8db8b766d60b88f80
sergey <szaprudnov@hotmail.com>
2023-04-06 12:37:29 +0300

    рефакторинг

--- src/video.py
+++ src/video.py
@@ -5,31 +5,23 @@ class Video:
 
     def __init__(self, id_video: str) -> None:
         self.id_video = id_video
-
-    @property
-    def youtube(self):
-        return Channel.get_service().videos().list(
-            part='snippet,statistics,contentDetails,topicDetails', id=self.id_video
-        ).execute()
-
-    @property
-    def name(self):
-        return self.youtube.get('items')[0].get('snippet').get('title')
-
-    @property
-    def url(self):
-        return 'https://www.youtube.com/watch?v=' + self.youtube.get('items')[0].get('id')
-
-    @property
-    def view_count(self):
-        return self.youtube.get('items')[0].get('statistics').get('viewCount')
-
-    @property
-    def like_count(self):
-        return self.youtube.get('items')[0].get('statistics').get('likeCount')
-
+        try:
+            self.youtube = Channel.get_service().videos().list(
+                    part='snippet,statistics,contentDetails,topicDetails', id=self.id_video
+            ).execute()
+            self.title = self.youtube.get('items')[0].get('snippet').get('title')
+            self.url = 'https://www.youtube.com/watch?v=' + self.youtube.get('items')[0].get('id')
+            self.viev_count = self.youtube.get('items')[0].get('statistics').get('viewCount')
+            self.like_count = self.youtube.get('items')[0].get('statistics').get('likeCount')
+        except IndexError:
+            print('Не верная ссылка!!!')
+            self.youtube = None
+            self.title = None
+            self.url = None
+            self.viev_count = None
+            self.like_count = None
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
 
 
 class PLVideo(Video):
