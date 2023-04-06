2e7af9420bf2fa2303de4d09b4cf591ef7cac868
sergey <szaprudnov@hotmail.com>
2023-04-06 12:39:52 +0300

    add YT_API_KEY

--- src/channel.py
+++ src/channel.py
@@ -7,7 +7,7 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    youtube = build('youtube', 'v3', developerKey='AIzaSyAadr26UgIsn6iQWS9uITSr7wV0_h7JYfE')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
